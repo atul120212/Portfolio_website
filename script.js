e5e6c78e290628589ca13a22eade81870517d476
Atul Sharma <87241497+atul120212@users.noreply.github.com>
2024-03-18 23:19:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "Community Lead", "Open Source Contributor"],
+    strings: ["Django Developer", "Community Leader", "Open Source Contributor"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
